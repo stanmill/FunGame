b1e044fe00abea7e3f5b339b008cec726ffd58bd
stanmillz <stanley.pena@niocoders.com>
2019-07-17 13:19:20 -0400

    update

--- FunGame/main.cpp
+++ FunGame/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h> // engine library
+#include <SDL2/SDL_image.h> //image library
+
+using namespace std;
+
+const int WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // creating a window    
+    
+    
+    // check if the window is created sucessfully
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        cout << "Error" << SDL_GetError()<< std::endl;
+    }
+    
+    SDL_Window *window = SDL_CreateWindow("Hello window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    if(NULL == window){
+        cout << "unable to create window";
+        return EXIT_FAILURE;
+    }
+    
+    
+    SDL_Event windowEvent;
+    
+    while (true) {
+        if(SDL_PollEvent(&windowEvent)){
+            if (SDL_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
+    
 }
